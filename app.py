7029238282a40da1d1428f2b85fe676a9641b2ea
IsabelXue15 <isabelxue@college.harvard.edu>
2024-08-13 21:39:50 -0400

    updated app

--- app.py
+++ app.py
@@ -58,23 +58,6 @@ with st.form("recordpitch"):
                 writer.writerows(rows)
             csvfile.close()
 
-# with st.form("my_form"):
-#     text = st.text_area(
-#         "Enter text:",
-#         "What are the three key pieces of advice for learning how to code?",
-#     )
-#     submitted = st.form_submit_button("Submit")
-#     if not openai_api_key.startswith("sk-"):
-#         st.warning("Please enter your OpenAI API key!", icon="⚠")
-#     if submitted and openai_api_key.startswith("sk-"):
-#         reader = PdfReader(pitch)
-#         n = 0
-#         pitch = ""
-#         for page in reader.pages:
-#             pitch += page.extract_text()
-#         generate_response(text + pitch)
-
-
     
 
 
